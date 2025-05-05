1bc74934adb5091c6814be7f459e6d102b1b4c36
mxustin <mxustin@gmail.com>
2025-05-05 22:41:23 +0300

    Подготовлена структура файлов и папок для разработки компонента AppShell.

---
+++ src/components/organisms/AppShell/AppShell.tsx
@@ -0,0 +1,42 @@
+// Визуальный компонент-организм AppShell (заголовок главного окна приложения)
+
+/**
+ * Организм: AppShell - адаптивный интерфейс приложения, который включает в себя: заголовок (header), навигационную
+ *   панель (navbar), боковую панель (aside) и подвал (footer).
+ * @module
+ */
+
+import { AppShell, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+function BaseLayout() {
+    const [opened, { toggle }] = useDisclosure();
+
+    return (
+        <AppShell
+            header={{ height: 60 }}
+            navbar={{
+                width: 300,
+                breakpoint: 'sm',
+                collapsed: { mobile: !opened },
+            }}
+            padding="md"
+        >
+            <AppShell.Header>
+                <Burger
+                    opened={opened}
+                    onClick={toggle}
+                    hiddenFrom="sm"
+                    size="sm"
+                />
+                <div>Logo</div>
+            </AppShell.Header>
+
+            <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+            <AppShell.Main>Main</AppShell.Main>
+        </AppShell>
+    );
+}
+
+export default BaseLayout;
