4cdd1bd8d1c40f24b71a1bb955b393f0f29a5ae5
mxustin <mxustin@gmail.com>
2025-05-07 22:36:12 +0300

    Оптимизированы импорты путем добавления алиаса для пути './src'.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 // Файл конфигурации Vite, определяющий параметры сборки для проекта
 
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url';
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+    ],
+  }
 })
