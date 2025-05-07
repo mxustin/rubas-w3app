e05c77af6d0e8c6b284cfa85c422e87fa01b4901
mxustin <mxustin@gmail.com>
2025-05-07 22:25:34 +0300

    Обновлены настройки линтера.

--- eslint.config.js
+++ eslint.config.js
@@ -14,31 +14,47 @@
  * export default tseslint.config(...)
  */
 
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import importPlugin from 'eslint-plugin-import';
+import globals from 'globals';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    {
+        ignores: ['dist'],
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+    {
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            parser: tseslint.parser, // ✅ обязательно указать парсер
+            parserOptions: {
+                sourceType: 'module',
+                ecmaVersion: 2020,
+            },
+            globals: globals.browser,
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            'simple-import-sort': simpleImportSort,
+            'import': importPlugin,
+        },
+        rules: {
+// React
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+            'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+            // Сортировка импортов
+            'simple-import-sort/imports': 'warn',
+            'simple-import-sort/exports': 'warn',
+
+            // Валидация импортов
+            'import/first': 'error',
+            'import/newline-after-import': 'warn',
+            'import/no-duplicates': 'warn',
+        },
     },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+);
