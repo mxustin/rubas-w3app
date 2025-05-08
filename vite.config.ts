069f367be09d31ebf9cfa38ea7612715e5b519f0
mxustin <mxustin@gmail.com>
2025-05-08 23:43:15 +0300

    Удален лишний комментарий.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
