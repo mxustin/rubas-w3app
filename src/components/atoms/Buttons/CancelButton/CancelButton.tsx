a503ac6cc81801c7ed7759997e59bb2c9b2d5e44
mxustin <mxustin@gmail.com>
2025-05-08 23:34:57 +0300

    Документация перенесена и расположена непосредственно перед компонентом; удалена мемоизация (из-за нее не работала документация).

--- src/components/atoms/Buttons/CancelButton/CancelButton.tsx
+++ src/components/atoms/Buttons/CancelButton/CancelButton.tsx
@@ -1,23 +1,4 @@
-// Атомарный компонент: кнопка "Отмена" [★★★★☆]
-
-/**
- * CancelButton - атомарная кнопка на базе Ant Design для отмены действий, закрытия модальных окон без сохранения
- * результатов, или для прерывания каких-либо процессов
- * 
- * @component CancelButton
- * @category Atoms
- * @param {() => void} onCancel - Колбэк для обработки отмены действия
- * @param {React.ReactNode} [children] - Кастомное содержимое кнопки
- * @example <CancelButton onCancel={handleCancel}>Отмена</CancelButton>
- * 
- * Кнопка является атомарным элементом (Atomic Design) и предназначена для прерывания каких-либо процессов или отмены
- * каких-либо действий. Можно передавать любые свойства, поддерживаемые AntD Button (disabled, icon и т.д.).
- *
- * Поддерживает автоматическую логику локализации текста, переданного в children, с помощью i18n. Для перевода текста 
- * используется ключ из props.children. Возможно использование кастомных надписей
- *
- * Поддерживает логирование при нажатии.
- */
+// Атомарный компонент: кнопка "Отмена" [★★★★★]
 
 import { Button, type ButtonProps } from 'antd';
 import React from 'react';
@@ -27,7 +8,21 @@ import log from '@/log';
 
 import classes from './CancelButton.module.scss';
 
-const RawCancelButton: React.FC<ButtonProps & { onCancel: () => void }> = (props) => {
+/**
+ * CancelButton - атомарная кнопка на базе Ant Design для отмены действий, закрытия модальных окон без сохранения
+ * результатов, или для прерывания каких-либо процессов. Кнопка является атомарным элементом (Atomic Design) и
+ * предназначена для прерывания каких-либо процессов или отмены каких-либо действий. Можно передавать любые свойства,
+ * поддерживаемые AntD Button (disabled, icon и т.д.). Поддерживает автоматическую логику локализации текста,
+ * переданного в children, с помощью i18n. Для перевода текста используется ключ из props.children. Возможно
+ * использование кастомных надписей. Поддерживает логирование при нажатии.
+ *
+ * @component CancelButton
+ * @category Atoms
+ * @example
+ *   <CancelButton onCancel={handleCancel}>Отмена</CancelButton>
+ */
+
+export const CancelButton: React.FC<ButtonProps & { onCancel: () => void }> = (props) => {
     const { t } = useTranslation();
     const componentName = 'CancelButton';
 
@@ -49,5 +44,3 @@ const RawCancelButton: React.FC<ButtonProps & { onCancel: () => void }> = (props
         </Button>
     );
 };
-
-export const CancelButton = React.memo(RawCancelButton);
